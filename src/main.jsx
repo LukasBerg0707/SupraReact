c083f6f84851c29f438c0da82daef9b30e9daf29
LukasBerg0707 <lukasberg0707@gmail.com>
2025-02-13 12:11:08 +0100

    First commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import QuoteApp from './Quote'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<QuoteApp />)
